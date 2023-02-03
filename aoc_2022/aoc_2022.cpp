295dafaf79874dc01564e9538fbfdb3c1272c0dc
rowlesmr <rowlesmr@gmail.com>
2023-02-03 21:49:19 +0800

    ten

--- aoc_2022/aoc_2022.cpp
+++ aoc_2022/aoc_2022.cpp
@@ -11,6 +11,12 @@
 #include <iterator>
 #include <array>
 #include <unordered_map>
+#include <cmath>
+#include <unordered_set>
+#include <functional>
+
+
+#if 0
 
 std::vector<std::string> split(const std::string& s)
 {
@@ -664,7 +670,7 @@ int six()
 	std::array<char, MAGIC_NUM> chars;
 	size_t ic{ 0 };
 	char ch;
-	size_t char_num{ 1 };
+	int char_num{ 1 };
 	while (file_in.get(ch))
 	{
 		chars[ic++] = ch;
@@ -699,7 +705,7 @@ int six_2()
 	std::array<char, MAGIC_NUM> chars;
 	size_t ic{ 0 };
 	char ch;
-	size_t char_num{ 1 };
+	int char_num{ 1 };
 	while (file_in.get(ch))
 	{
 		chars[ic++] = ch;
@@ -724,27 +730,29 @@ int six_2()
 
 struct Node
 {
-	Node* parent;
-	int size{ 0 };
+	Node* parent{ nullptr };
+	int size{ 0 }; //represents the file size or the total size of the directory and all subdirs/files.
 	bool isDir{ true };
-	std::string name;
-	std::list<Node> children;
+	std::string name{};
+	std::list<Node> children; //std::list maintains pointer validity - no resize. std::vector invalidates pointers to elements upon reallocation.
 
 	Node* Parent()
 	{
 		if (parent == nullptr)
-			return this;
+		{
+			return this; // can't ever get a nullptr returned. If we go to the parent of the root, we get the root.
+		}
 		return parent;
 	}
 
-	int calculate_total_size()
+	int calculate_total_size() //of everything contained within this node
 	{
 		if (size > 0)
 			return size;
 		//otherwise
 		for (Node& c : children)
 		{
-			size += c.calculate_total_size();
+			size += c.calculate_total_size(); //this updates the size of directories
 		}
 		return size;
 	}
@@ -804,7 +812,7 @@ int seven()
 			char dollar;
 			std::string command;
 			std::string arg;
-			file_in >> dollar >> command;
+			file_in >> dollar >> command; //dollar is just there to consume a token. We don't need to do anything with it.
 
 			if (command == "cd")
 			{
@@ -818,7 +826,7 @@ int seven()
 					currentDir = currentDir->Parent();
 				}
 				else
-				{ // going into a directory'
+				{ // going into a directory
 					for (Node& n : currentDir->children)
 					{
 						if (n.name == arg)
@@ -843,11 +851,10 @@ int seven()
 			if (!file_in) break;
 
 			//if this is true, then we've stopped listing files and have issued a command
-			// this backs up the stream, and we flag it to go to command mode
 			if (command == "$")
 			{
 				file_in.seekg(-2, std::ios::cur); //from the current position, go back 2; ie, the ' ' and the '$'
-				commandMode = true;
+				commandMode = true; // we're now issuing commands
 			}
 			else if (command == "dir") //we need to add a directory to the children list
 			{
@@ -863,7 +870,7 @@ int seven()
 				currentDir->children.push_back({ currentDir, size, false, name });
 			}
 		}
-	}
+	}//end of file
 
 	tree.calculate_total_size();
 
@@ -895,6 +902,467 @@ int seven()
 
 
 
+bool isVisible(const std::vector<std::vector<int>>& forest, int m, int n)
+{
+	int val = forest[m][n];
+
+	bool up{ true };
+	bool down{ true };
+	bool left{ true };
+	bool right{ true };
+
+	for (int i{ m - 1 }; i >= 0; --i)
+	{
+		if (val <= forest[i][n])
+		{
+			up = false;
+			break;
+		}
+	}
+	for (int i{ m + 1 }; i < forest.size(); ++i)
+	{
+		if (val <= forest[i][n])
+		{
+			down = false;
+			break;
+		}
+	}
+	for (int i{ n - 1 }; i >= 0; --i)
+	{
+		if (val <= forest[m][i])
+		{
+			left = false;
+			break;
+		}
+	}
+	for (int i{ n + 1 }; i < forest[m].size(); ++i)
+	{
+		if (val <= forest[m][i])
+		{
+			right = false;
+			break;
+		}
+	}
+	return  up || down || left || right;
+}
+
+
+int eight()
+{
+	std::ifstream file_in("8.txt");
+	if (!file_in)
+	{
+		std::cout << "Couldn't open file.\n";
+		return -1;
+	}
+
+	std::string line;
+
+
+	std::vector<std::vector<int>> forest{};
+	size_t row{ 0 };
+	while (std::getline(file_in, line))
+	{
+		forest.emplace_back();
+		for (size_t i{ 0 }; i < line.size(); ++i)
+		{
+			forest[row].push_back(line[i] - '0');
+		}
+		++row;
+	}
+
+	size_t visible{ 2 * forest.size() + 2 * forest[0].size() - 4 };
+
+	for (size_t i{ 1 }; i < forest.size() - 1; ++i)
+	{
+		for (size_t j{ 1 }; j < forest[i].size() - 1; ++j)
+		{
+			if (isVisible(forest, i, j))
+			{
+				++visible;
+			}
+		}
+	}
+
+
+
+	return visible;
+}
+
+
+
+int scenicScore(const std::vector<std::vector<int>>& forest, int m, int n)
+{
+	int val = forest[m][n];
+
+	int up{ 0 };
+	int down{ 0 };
+	int left{ 0 };
+	int right{ 0 };
+
+	for (int i{ m - 1 }; i >= 0; --i)
+	{
+		++up;
+		if (val <= forest[i][n])
+		{
+			break;
+		}
+	}
+	for (int i{ m + 1 }; i < forest.size(); ++i)
+	{
+		++down;
+		if (val <= forest[i][n])
+		{
+			break;
+		}
+	}
+	for (int i{ n - 1 }; i >= 0; --i)
+	{
+		++left;
+		if (val <= forest[m][i])
+		{
+			break;
+		}
+	}
+	for (int i{ n + 1 }; i < forest[m].size(); ++i)
+	{
+		++right;
+		if (val <= forest[m][i])
+		{
+			break;
+		}
+	}
+
+	return up * down * left * right;
+}
+
+
+
+int eight_2()
+{
+	std::ifstream file_in("8.txt");
+	if (!file_in)
+	{
+		std::cout << "Couldn't open file.\n";
+		return -1;
+	}
+
+	std::string line;
+
+
+	std::vector<std::vector<int>> forest{};
+	size_t row{ 0 };
+	while (std::getline(file_in, line))
+	{
+		forest.emplace_back();
+		for (size_t i{ 0 }; i < line.size(); ++i)
+		{
+			forest[row].push_back(line[i] - '0');
+		}
+		++row;
+	}
+
+	int maxScore{ 0 };
+	int tmp;
+	for (size_t i{ 1 }; i < forest.size() - 1; ++i)
+	{
+		for (size_t j{ 1 }; j < forest[i].size() - 1; ++j)
+		{
+			if ((tmp = scenicScore(forest, i, j)) > maxScore)
+			{
+				maxScore = tmp;
+			}
+		}
+	}
+
+	return maxScore;
+}
+#endif
+
+int sign(int val)
+{
+	if (val > 0) return 1;
+	if (val == 0) return 0;
+	return -1;
+}
+
+
+struct Point
+{
+	int x{ 12 };
+	int y{ 6 };
+
+	void up()
+	{
+		++y;
+	}
+
+	void down()
+	{
+		--y;
+	}
+
+	void left()
+	{
+		--x;
+	}
+
+	void right()
+	{
+		++x;
+	}
+
+	void move(const char direction)
+	{
+		if (direction == 'R')
+		{
+			right();
+		}
+		else if (direction == 'L')
+		{
+			left();
+		}
+		else if (direction == 'U')
+		{
+			up();
+		}
+		else if (direction == 'D')
+		{
+			down();
+		}
+	}
+
+	[[nodiscard]] bool isCloseTo(const Point other) const
+	{
+		return (std::abs(this->x - other.x) <= 1 && std::abs(this->y - other.y) <= 1);
+	}
+
+	void makeCloseTo(const Point other)
+	{
+		if (isCloseTo(other))
+			return;
+
+		if (this->x == other.x && this->y != other.y)
+		{
+			int y_diff{ this->y - other.y };
+			this->y = other.y + sign(y_diff);
+		}
+		else if (this->x != other.x && this->y == other.y)
+		{
+			int x_diff{ this->x - other.x };
+			this->x = other.x + sign(x_diff);
+		}
+		else //different in both dimension
+		{
+			//the point moves diagonally to catch up
+			int x_diff{ sign(other.x - this->x) };
+			int y_diff{ sign(other.y - this->y) };
+
+			this->x += x_diff;
+			this->y += y_diff;
+		}
+	}
+};
+
+
+std::ostream& operator<<(std::ostream& os, const Point& p)
+{
+	os << '(' << p.x << ',' << p.y << ')';
+	return os;
+}
+
+bool operator==(const Point& left, const Point& other)
+{
+	return (left.x == other.x) && (left.y == other.y);
+}
+
+
+struct Point_Hash
+{
+	inline size_t operator() (const Point& point) const
+	{
+		return point.x ^ point.y;
+	}
+};
+
+void drawPoints(Point head, Point tail, int i = 6, int j = 5)
+{
+	std::cout << "\n";
+	for (int jj = j; jj >= 1; --jj)
+	{
+		for (int ii = 1; ii <= i; ++ii)
+		{
+			Point tmp{ ii, jj };
+			if (tmp == head)
+				std::cout << 'H';
+			else if (tmp == tail)
+				std::cout << 'T';
+			else
+			{
+				if (ii == 1 && jj == 1)
+					std::cout << 's';
+				else
+					std::cout << '.';
+			}
+		}
+		std::cout << '\n';
+	}
+	std::cout << "\n";
+}
+
+template<typename Container>
+void drawPoints(Container knots, int i = 26, int j = 21)
+{
+	std::cout << "\n";
+	for (int jj = j; jj >= 1; --jj)
+	{
+		for (int ii = 1; ii <= i; ++ii)
+		{
+			bool matched{ false };
+			Point tmp{ ii, jj };
+			for (int k{ 0 }; k < knots.size(); ++k)
+			{
+				if (tmp == knots[k])
+				{
+					matched = true;
+					if (k == 0)
+						std::cout << 'H';
+					else
+						std::cout << k;
+					break;
+				}
+			}
+			if (!matched)
+			{
+				if (ii == 12 && jj == 6)
+					std::cout << 's';
+				else
+					std::cout << '.';
+			}
+		}
+		std::cout << '\n';
+	}
+	std::cout << "\n";
+}
+
+
+int nine()
+{
+	std::ifstream file_in("9.txt");
+	if (!file_in)
+	{
+		std::cout << "Couldn't open file.\n";
+		return -1;
+	}
+
+	char direction;
+	int distance;
+
+	Point head{};
+	Point tail{};
+
+	std::unordered_set<Point, Point_Hash> tails{ tail };
+	while (file_in)
+	{
+		file_in >> direction >> distance;
+
+		if (!file_in) break;
+		for (int i{ 0 }; i < distance; ++i)
+		{
+			head.move(direction);
+			tail.makeCloseTo(head);
+			tails.emplace(tail);
+		}
+	}//end of file
+
+
+	return tails.size();
+}
+
+
+int nine_2()
+{
+	std::ifstream file_in("9.txt");
+	if (!file_in)
+	{
+		std::cout << "Couldn't open file.\n";
+		return -1;
+	}
+
+	char direction;
+	int distance;
+
+	const int KNOTS{ 10 };
+	std::array<Point, KNOTS> knots{};
+
+	std::unordered_set<Point, Point_Hash> tails{ knots[KNOTS - 1] };
+	//drawPoints(knots);
+	while (file_in)
+	{
+		file_in >> direction >> distance;
+		//std::cout << direction << distance << '\n';
+		if (!file_in) break;
+		for (int i{ 0 }; i < distance; ++i)
+		{
+			knots[0].move(direction);
+
+			for (int k = 1; k < KNOTS; ++k)
+			{
+				knots[k].makeCloseTo(knots[k - 1]);
+			}
+			tails.emplace(knots[KNOTS - 1]);
+		}
+		//drawPoints(knots);
+	}//end of file
+
+
+	return tails.size();
+}
+
+
+
+int ten()
+{
+	std::ifstream file_in("10_test.txt");
+	if (!file_in)
+	{
+		std::cout << "Couldn't open file.\n";
+		return -1;
+	}
+
+	std::string operation{};
+	int value{ 0 };
+	int cycle{ 0 };
+	int tot_value{ 1 };
+
+	while (file_in)
+	{
+		++cycle;
+		file_in >> operation;
+		if (operation == "addx"){
+			file_in >> value;
+
+			if(cycle == 20 || (cycle-20)/40 == 0){
+				std::cout << cycle << " : " << tot_value << '\n';
+			}
+
+			tot_value += value;
+			++cycle;
+		}
+		// do things for the noop
+
+		
+
+
+		
+	}//end of file
+
+
+	return tot_value;
+}
+
+
+
 int main()
 {
 	//std::cout << " 1 : " << one() << '\n';
@@ -915,7 +1383,15 @@ int main()
 	//std::cout << " 6 : " << six() << '\n';
 	//std::cout << " 6x: " << six_2() << '\n';
 
-	std::cout << " 7 : " << seven() << '\n';
+	//std::cout << " 7 : " << seven() << '\n';
+
+	//std::cout << " 8 : " << eight() << '\n';
+	//std::cout << " 8x: " << eight_2() << '\n';
+
+	//std::cout << " 9 : " << nine() << '\n';
+	//std::cout << " 9 : " << nine_2() << '\n';
+
+	std::cout << "10 : " << ten() << '\n';
 
 	return 0;
 }
